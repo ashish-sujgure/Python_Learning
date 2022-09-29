f6df480549d6657bd8b7f88a9983e8850ac8e464
AshishSujgure <ashish.sujgure@pivotree.com>
2022-09-29 20:51:09 +0530

    Added day 2 hw practice code and notes

---
+++ Day_2_hw.py
@@ -0,0 +1,35 @@
+'''
+capital_states= ["Bhopal","Mumbai","Imphal","Shillong","Aizawl","Kohima"]
+
+
+1. FIND THE LENGHT OF LIST capital_states
+
+2. PRINT BELOW ITEMS USING INDEXING IN THE LIST  capital_states
+OUTPUT SHOULD BE : "Imphal"
+OUTPUT SHOULD BE : "Kohima"
+
+3. FIND THE LENGHT OF LIST 
+z = ['',"","","",""]
+z = [None]
+'''
+
+capital_states= ["Bhopal","Mumbai","Imphal","Shillong","Aizawl","Kohima"]
+print("=============================")
+print("Length of List cpaital_states")
+print("=============================")
+print(len(capital_states))
+
+print("=============================")
+print("Print Imphal and Kohima using index")
+print("=============================")
+print(capital_states[2])
+print(capital_states[5])
+
+print("=============================")
+print("Length of list z")
+print("=============================")
+
+z=["","","","",""]
+print(len(z))
+z=[None]
+print(len(z))
